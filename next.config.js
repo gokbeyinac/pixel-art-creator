896974f3ae565d30f80f22f573cc48798f5d3b41
NGokbey <63238743+gokbeyinac@users.noreply.github.com>
2025-02-26 19:34:37 +0300

    Switch to Netlify configuration

--- next.config.js
+++ next.config.js
@@ -3,13 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'export',
-  assetPrefix: '/pixel-art-creator/',
-  basePath: '/pixel-art-creator',
   images: {
     unoptimized: true,
   },
-  // GitHub Pages için trailing slash ekle
-  trailingSlash: true,
 };
 
 module.exports = nextConfig; 
